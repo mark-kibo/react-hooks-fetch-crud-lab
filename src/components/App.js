1448aee562587794451728458e51737ef59f5547
mark_kibo <kibochamark@gmail.com>
2023-08-25 13:10:01 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,59 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  let [questions, setQuestions] = useState()
+
+  // fetch data
+  const getData=async ()=>{
+    const response= await fetch("http://localhost:4000/questions")
+    const data=await response.json()
+    return data
+  }
+  // post data
+  
+  // fetch data
+  const postFormData=async (questionData)=>{
+    let options={
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json",
+        accept:"application/json"
+      },
+      body:JSON.stringify(questionData)
+
+    }
+    const response= await fetch("http://localhost:4000/questions", options)
+    const data=await response.json()
+    return data
+  }
+
+
+  // use effect
+  useEffect(()=>{
+    getData()
+    .then(myData=>{
+      console.log(myData)
+      // setQuestions(myData)
+    }).catch(e=>console.log(e))
+  }, [])
+
+  // function addDataToList(data){
+  //   let ourPostData=postFormData(data)
+  //   console.log(ourPostData)
+  //   let ourNewData=[...questions, ourPostData]
+  //   setQuestions(ourNewData)
+  // }
+
+
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm  /> : <QuestionList />}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,17 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({myQuestions}) {
+  console.log(myQuestions)
+  let questionTag=myQuestions.map((myQuestion)=>{
+    return <QuestionItem key={myQuestion.id} question={myQuestion}/>
+  })
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questionTag}
+        {/* display QuestionItem components here after fetching */}</ul>
     </section>
   );
 }
